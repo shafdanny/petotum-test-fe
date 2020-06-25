bf96f1be24b6a55bb480171e8043fb1ee6ed3a83
Shafiq Daniel <shafdanny@gmail.com>
2020-06-25 23:30:34 +0800

    Show static text list

---
+++ src/components/Box.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container class="grey lighten-5">
+    <v-row v-for="rowIndex in 4" :key="rowIndex">
+      <v-col v-for="n in 4" :key="n" cols="3">
+        <v-card
+          class="card pa-2"
+          outlined
+          tile
+          v-if="getBoxContent(getPosition(rowIndex, n))"
+        >{{ getBoxContent(getPosition(rowIndex, n)).text }}</v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Box",
+  props: ["boxContents"],
+  methods: {
+    getBoxContent: function(position) {
+      console.log("Getting box content in position " + position);
+      return this.boxContents.find(element => element.position == position);
+    },
+    getPosition: function(rowIndex, colIndex) {
+      return (rowIndex - 1) * 4 + colIndex;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card {
+  text-align: center;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
